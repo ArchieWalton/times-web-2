fdb8694b434f7ffa8ad8f027e2c4601c93358d96
ParadoxEpcot <59997116+ParadoxEpcot@users.noreply.github.com>
2020-01-30 14:18:51 +0000

    Update src.js

--- js/src.js
+++ js/src.js
@@ -1,4 +1,4 @@
 
-document.getElementById("20").innerHTML = "TEST";
+document.getElementById("20").innerHTML = "TEST2";
 var testvar = new themeparks.Parks.WaltDisneyWorldEpcot();
 document.getElementById("21").innerHTML = "TEST";
